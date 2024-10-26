d46f5e5fea40bba7e2032634bc232e5e1e6d51eb
drobin04 <90974129+drobin04@users.noreply.github.com>
2024-10-25 22:51:14 -0500

    development started

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+void wr(string x) {Console.WriteLine(x);}
+
+// Display menu items - 
+// Display whether a dotnet app is found in the current directory...
+
+wr("1. Create new project in this directory");
+wr("2. Create new project in sub directory");
+wr("Open a project...");
+wr("3. Compile & Run Current Project");
+//wr("");
+wr("Press any key to continue....");
+Console.ReadKey();
